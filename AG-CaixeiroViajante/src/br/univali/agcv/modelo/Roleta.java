eb6cb9227b38e09f9a81ebdd1dc1eaa7bcec48d0
AiltonJr <ailtoncardoso0@gmail.com>
2016-06-25 20:49:46 -0300

    Adicionado uma ideia de como resolver o problema do perentual de aptidão

--- AG-CaixeiroViajante/src/br/univali/agcv/modelo/Roleta.java
+++ AG-CaixeiroViajante/src/br/univali/agcv/modelo/Roleta.java
@@ -7,21 +7,27 @@ import java.util.Random;
 public class Roleta {
     private List<Integer> roleta;
     private Random rand;
+    private double totalFitnes;
 
     public Roleta(List<Rota> populacao) {
         roleta = new ArrayList();
         rand = new Random();
         
+        for (Rota individui : populacao) {
+            totalFitnes += individui.getFitness()*100;
+        }
+        
         for (int i=0; i < populacao.size(); i++) {
-            for (int j=0; j < populacao.get(i).getFitness(); j++) {
+            int percent = (int) Math.round(((populacao.get(i).getFitness()*100)/totalFitnes)*100); // Vai retornar um percentual arredondado
+            for (int j=0; j < percent; j++) {
                 roleta.add(i);
             }
+            System.out.println("");
         }
+        System.out.println("");
     }
     
     public int getSorteadoIndex() {
-        int numero = rand.nextInt(roleta.size());
-        System.out.println(numero);
-        return numero;
+        return roleta.get(rand.nextInt(roleta.size()));
     }
 }
